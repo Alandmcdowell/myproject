8c94e4d3934d2dceb0e18affbd2070496ea8617d
alandmcdowell <collaborall@outlook.com>
2014-01-28 11:08:17 +0000

    user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: 'collaborall@outlook.com', password: '123456', profile_attributes: { name: 'Name here' })
